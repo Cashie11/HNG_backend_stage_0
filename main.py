ad73ad457f55ef5e63220a9b876817b320417b71
Cashie11 <frankizuchukwu094@gmail.com>
2025-02-03 19:56:31 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI
+from datetime import datetime
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+# Enable CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  
+    allow_methods=["*"],  
+    allow_headers=["*"], 
+    allow_credentials=True, 
+)
+
+@app.get("/")
+def get_info():
+    return {
+        "email": "frankizuchukwu094@gmail.com",  # my email
+        "current_datetime": datetime.utcnow().isoformat() + "Z",
+        "github_url": "https://github.com/Cashie11/weather_api.git",  # my Git repo
+    }
